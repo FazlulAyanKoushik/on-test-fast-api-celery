c8c5fbdf265456a133da67fc95b8ea294c897ad6
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2025-07-22 15:37:20 +0600

    configure environment variables for email settings and add python-decouple dependency

--- app/email_settings.py
+++ app/email_settings.py
@@ -1,10 +1,11 @@
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
+from decouple import config
+
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_RECIPIENTS = [
-    "@gmail.com",
-    "@gmail.com",
-    "@gmail.com",
-    "@diu.edu.bd"
+    "kf.ayan17@gmail.com",
+    "jbc.nayeem@gmail.com",
+    "ayon15-7527@diu.edu.bd"
 ]
 EMAIL_SUBJECT = "Test Email from FastAPI Celery"
 EMAIL_BODY = "This is a test email sent using FastAPI and Celery."
